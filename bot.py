e4d0a9af71fff0699e73b5fcda07287d8dbf5ce6
Shavkat Mustafoyev <shavkatmustafoyev5@gmail.com>
2023-02-05 20:17:36 +0500

    first commit

---
+++ bot.py
@@ -0,0 +1,40 @@
+"""
+This is an echo bot.
+It echoes any incoming text messages.
+"""
+
+import logging
+import wikipedia
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '6107368266:AAHok85fzgRfEu0ndZwjIhR9uCyw9ajMSbY'
+wikipedia.set_lang("uz")
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Xush kelibsiz")
+
+
+
+@dp.message_handler()
+async def sendWiki(message: types.Message):
+   try:
+       respond = wikipedia.summary(message.text)
+       await message.answer(respond)
+   except:
+       await message.answer("Bu mavzuga oid maqola topilmadi")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
